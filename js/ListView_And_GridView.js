7a3d627768816bcf6309ee1a0a230a488a94269b
mohannad sroor <mohannadsroor181@gmail.com>
2024-12-12 19:52:23 +0300

    Initial commit

---
+++ js/ListView_And_GridView.js
@@ -0,0 +1,20 @@
+function full(){
+    document.getElementById('column1').style.width='100%';
+    document.getElementById('column2').style.width='100%';
+    document.getElementById('column3').style.width='100%';
+    document.getElementById('column4').style.width='100%';
+}
+
+function towcColumn(){
+    document.getElementById('column1').style.width='50%';
+    document.getElementById('column2').style.width='50%';
+    document.getElementById('column3').style.width='50%';
+    document.getElementById('column4').style.width='50%';
+}
+
+function fourColumn(){
+    document.getElementById('column1').style.width='25%';
+    document.getElementById('column2').style.width='25%';
+    document.getElementById('column3').style.width='25%';
+    document.getElementById('column4').style.width='25%';
+}
